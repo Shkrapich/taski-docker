e7d8181c6b935bc703eb836fcbdadb4b43800537
Shkrapich <lol2523338@yandex.ru>
2025-01-30 23:08:30 +0300

    pep82

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,9 +1,13 @@
+"""This is the init file for the api package."""
 from django.contrib import admin
 
 from .models import Task
 
 
 class TaskAdmin(admin.ModelAdmin):
+
+   """This is the init file for the api package."""
+    
     list_display = ('title', 'description', 'completed')
 
 
